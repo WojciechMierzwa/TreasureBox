e1e219ac9244946e063cb2c93c29f7bbac23e180
Wojciech Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2025-04-21 15:47:51 +0200

    Backend setup

---
+++ backend/src/main/java/com/example/treasurebox/TreasureBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.treasurebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasureBoxApplication.class, args);
+	}
+
+}
