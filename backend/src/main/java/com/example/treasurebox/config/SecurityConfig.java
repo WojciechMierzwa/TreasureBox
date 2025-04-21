853af040f0b21e171bc98d4f3bd62f8c6730e885
Wojciech Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2025-04-21 20:07:52 +0200

    Frontend initilized, basic config front<->back<->DB, login copomnent added

---
+++ backend/src/main/java/com/example/treasurebox/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.treasurebox.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import LoginPage from './components/LoginPage';
+
+
+
+export default function App() {
+  return (
+    <LoginPage/>
+  )
+}
